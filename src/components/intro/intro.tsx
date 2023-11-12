c197e1e8110c2aba043004190e2478cb16ceea06
sherzod555 <wiut00007584@gmail.com>
2023-11-12 23:27:32 +0500

    bg added

--- src/components/intro/intro.tsx
+++ src/components/intro/intro.tsx
@@ -1,8 +1,14 @@
-import bgImage from "../../assets/intro.png"
+// import bgImage from "../../assets/intro.png"
+import "./intro.css"
 
 export const Intro = () => {
     return (
         <>
+        <main>
+            <div className="bg_image w-screen h-auto">
+<p>cool</p>
+            </div>
+        </main>
        
         </>
     )
